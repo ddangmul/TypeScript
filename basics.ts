6fa63fa2cfd2d95fc696f3b8ff5c25b5da7b6af3
ddangmul <j.ddangmul@gmail.com>
2025-02-04 14:35:06 +0900

    배열 및 객체 타입

--- basics.ts
+++ basics.ts
@@ -18,3 +18,33 @@ useName = "Max";
 let isInstructor: boolean;
 
 isInstructor = true;
+
+// 문자열 배열
+let hobbies: string[];
+
+hobbies = ["Sports", "Cooking"];
+
+// 객체 타입(구조)
+// any : 타입스크립트는 특별 구성이 없어도 작동. 기본적으로 변수를 any 타입으로 간주. any는 알려줄 게 없다는 뜻이므로 어떤 유형도 허용. 단, 예비적으로 사용되는 타입이므로 지양하는 게 좋음
+// 객체 타입은 중괄호{}로 지정
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Max",
+  age: 32,
+};
+
+// person = {
+//   isEmplyee: true, // error
+// };
+
+// 객체 + 배열 타입
+let people: {
+  name: string;
+  age: number;
+}[];
+
+people = [person, { name: "sue", age: 29 }];
