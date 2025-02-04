a290c1d3845971be61735c61da750313a38d4d61
ddangmul <j.ddangmul@gmail.com>
2025-02-04 14:45:48 +0900

    union 타입

--- basics.ts
+++ basics.ts
@@ -11,9 +11,10 @@ let age: number;
 age = 12;
 age = 12.3;
 
-let useName: string;
+let useName: string | string[];
 
 useName = "Max";
+useName = ["a", "b"];
 
 let isInstructor: boolean;
 
@@ -52,6 +53,13 @@ people = [person, { name: "sue", age: 29 }];
 // [타입 추론] 타입스크립트의 핵심 기능
 // 타입스크립트는 명시적인 타입 표시가 없어도 기본적으로 가능한 많은 타입을 유추하려고 함
 // -> 개발자가 작성할 코드 감소하기 때문에, 가능한 타입 추론 사용을 권장
-let course = "React - The Complete Guide"; // 여기서 변수의 타입을 string으로 유추
+// let course = "React - The Complete Guide"; // 여기서 변수의 타입을 string으로 유추
 
 // course = 12341;  // error! 타입 추론 때문
+
+// [union 타입] 변수에 여러 타입을 지정
+// '|'로 타입 구분
+// 값과 타입을 유연하게 정의
+let course: string | number = "React - The Complete Guide";
+
+course = 1234;
