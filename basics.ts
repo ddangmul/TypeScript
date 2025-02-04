3bd1d07b3faa9f4bd0d614aa4bc531e851aab6a0
ddangmul <j.ddangmul@gmail.com>
2025-02-04 13:46:39 +0900

    기본 자료형

---
+++ basics.ts
@@ -0,0 +1,20 @@
+// [기본형 타입 Primitives]
+// number, string, boolean, [null, undefined, symbol]
+// arrays, objects
+// Function types, parameters
+
+// 변수명 뒤에 콜론 + 자료형 표기
+// 자료형은 소문자로 시작. 대문자로 시작하면 자바스크립트 객체를 가리킴
+
+let age: number;
+
+age = 12;
+age = 12.3;
+
+let useName: string;
+
+useName = "Max";
+
+let isInstructor: boolean;
+
+isInstructor = true;
