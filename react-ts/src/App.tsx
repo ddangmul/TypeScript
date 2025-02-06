747fa1c4778efa681eba6b736e9eafb18b00be86
ddangmul <j.ddangmul@gmail.com>
2025-02-06 12:00:30 +0900

    함수 props 작업

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,11 +1,15 @@
+import NewTodo from "./componsnts/NewTodo";
 import Todos from "./componsnts/Todos";
 import Todo from "./models/todo";
 
 function App() {
   const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
 
+  const addTodoHandler = (todoText: string) => {};
+
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
